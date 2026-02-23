0e72ea1dd1e86b29aff27b08f51deb98150db199
AntltNoProb <gingerant@sina.com>
2026-02-23 20:11:54 +0800

    Feat: add logo with transition to auth page

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet, useLocation } from "react-router"
 import styles from './AuthLayout.module.css'
+import { Avatar, Space } from "antd";
+import { motion } from 'framer-motion'
 
 const bgMap: Record<string, string> = {
-  '/auth/login': '0%',
-  '/auth/register': '100%',
+    '/auth/login': '0%',
+    '/auth/register': '100%',
 };
 
 export const AuthLayout = () => {
@@ -11,8 +13,19 @@ export const AuthLayout = () => {
     const curBgPos = bgMap[loc.pathname] ?? '0%';
 
     return <>
-    <div className={styles.background} style={{backgroundPositionX:curBgPos}}></div>
-    <div className={styles.container}>
-        <Outlet />
-    </div></>
+        <div className={styles.background} style={{ backgroundPositionX: curBgPos }}></div>
+        <div className={styles.container}>
+            <Space vertical>
+                <motion.div
+                    layout
+                    transition={{ duration: 0.2, ease: 'easeInOut' }}>
+                <Space className={styles["logo-container"]}>
+                    <Avatar size={70} src={<img src="/favicon.png" alt="Logo" />} className={styles.logo} ></Avatar>
+                    <span className={styles.title}>易宿酒店管理系统</span>
+                </Space>
+                </motion.div>
+                
+                    <Outlet />
+            </Space>
+        </div></>
 }
