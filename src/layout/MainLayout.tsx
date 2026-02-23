ad82e0fe83576bef80d72936063eabe1b71e03ae
AntltNoProb <gingerant@sina.com>
2026-02-23 23:41:06 +0800

    Feat: add mainlayout as framework of homepage

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,25 @@
+import { LayoutHeader } from "@/components/LayoutHeader";
+import { LayoutSider } from "@/components/LayoutSider";
+import { Layout } from "antd";
+import type { ItemType, MenuItemType } from "antd/es/menu/interface";
+import { Outlet } from "react-router";
 
+const { Content } = Layout;
 
-export const MainLayout = ()=><>
-</>
+interface MainLayoutProps {
+    menuItems?: ItemType<MenuItemType>[];
+}
+
+export function MainLayout({ menuItems }: MainLayoutProps) {
+    return (
+        <Layout style={{minHeight:'100vh'}}>
+            <LayoutHeader />
+            <Layout>
+                <LayoutSider menuItems={menuItems} />
+                <Content>
+                    <Outlet />
+                </Content>
+            </Layout>
+        </Layout>
+    );
+}
