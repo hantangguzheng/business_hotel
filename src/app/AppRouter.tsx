efa6f673ac13e90853937397738fa138a3129738
AntltNoProb <gingerant@sina.com>
2026-02-23 22:02:53 +0800

    Feat: add router gaurd to process auth for pages

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,11 +1,14 @@
 import { useAppSelector } from '@/hooks/hooks';
 import { AuthLayout } from '@/layout/AuthLayout';
+import { MainLayout } from '@/layout/MainLayout';
 import { LoginPage } from '@/pages/auth/LoginPage';
 import { RegisterPage } from '@/pages/auth/RegisterPage';
-import { createBrowserRouter, Navigate } from 'react-router';
+import type { AuthRole } from '@/types/auth';
+import { App } from 'antd';
+import { createBrowserRouter, Navigate, Outlet } from 'react-router';
 
-const RootRedirectionRouter = ()=>{
-    const {isAuthorized, isLoading, userobj} = useAppSelector((state)=>state.auth);
+const RootRedirectionRouter = () => {
+    const { isAuthorized, isLoading, userobj } = useAppSelector((state) => state.auth);
     if (isLoading) {
         return <div>loading@@</div>;
     }
@@ -18,28 +21,72 @@ const RootRedirectionRouter = ()=>{
     if (userobj?.role === 'MERCHANT') {
         return <Navigate to='/merchant' replace />;
     }
-    return null;
+    return <div>loading@</div>;
+}
+
+interface RoleGaurdProps {
+    allowedRoles: AuthRole[];
+}
+const RoleGaurd = ({ allowedRoles }: RoleGaurdProps) => {
+    const { isAuthorized, userobj, isLoading } = useAppSelector(s => s.auth);
+    const { message: msg } = App.useApp();
+    const msgLoadingKey = 'router-loading';
+
+    if (isLoading) {
+        msg.open({
+            content: '加载用户信息中...',
+            type: 'loading',
+            key: msgLoadingKey,
+            duration: 0,
+        });
+        return <div>loading@@</div>;
+    }
+    if (!isAuthorized) {
+        return <Navigate to="/auth/login" replace />;
+    }
+    if (!(userobj?.role) || !(allowedRoles.includes(userobj.role))) {
+        msg.open({
+            content: '用户权限不匹配',
+            type: 'error',
+            key: msgLoadingKey,
+            duration: 2,
+        });
+        return <Navigate to="/" replace />;
+    }
+    msg.destroy(msgLoadingKey);
+
+    return <Outlet />;
 }
 
 export const AppRouter = createBrowserRouter([
     {
-        path:'/',
-        element:<RootRedirectionRouter/>,
+        path: '/',
+        element: <RootRedirectionRouter />,
     },
     {
-        path:'/auth',
-        element: <AuthLayout/>,
-        children:[
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
             { index: true, element: <Navigate to="/auth/login" replace /> },
             {
-                path:'login',
+                path: 'login',
                 element: <LoginPage />
             },
             {
-                path:'register',
-                element:<RegisterPage />
+                path: 'register',
+                element: <RegisterPage />
             }
         ]
+    },
+    {
+        element: <RoleGaurd allowedRoles={['MERCHANT']} />,
+        children: [{
+            path: '/merchant',
+            element: <MainLayout />,
+            children: [
+
+            ],
+        },]
     }
     // todo: merchant and admin
 ])
