0131cb3feed493295891459c32f97525a660063e
AntltNoProb <gingerant@sina.com>
2026-02-24 20:38:19 +0800

    Feat: add merchant layout

---
+++ src/layout/MerchantLayout.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "@/layout/MainLayout"
+
+const merchantMenuItems = [
+    {key:'/merchant/hotels', label:'酒店信息管理'},
+    {key:'/merchant/hotels/create', label:'新增酒店申请'},
+];
+
+export const MerchantLayout = ()=>{
+
+    return <MainLayout menuItems={merchantMenuItems}/>
+}
