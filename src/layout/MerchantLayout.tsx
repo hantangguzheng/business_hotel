b64548a1ff296190228234b6d90647493e12473a
AntltNoProb <gingerant@sina.com>
2026-02-25 04:10:35 +0800

    Fix: url typo

--- src/layout/MerchantLayout.tsx
+++ src/layout/MerchantLayout.tsx
@@ -2,7 +2,7 @@ import { MainLayout } from "@/layout/MainLayout"
 
 const merchantMenuItems = [
     {key:'/merchant/hotels', label:'酒店信息管理'},
-    {key:'/merchant/hotels/create', label:'新增酒店申请'},
+    {key:'/merchant/hotel/create', label:'新增酒店申请'},
 ];
 
 export const MerchantLayout = ()=>{
